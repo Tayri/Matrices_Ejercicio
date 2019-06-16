e34252c7419e28ec927772350ea643e2b626d981
Mayantigot <Mayantigot@Bechingiwar>
2019-06-16 22:14:21 +0200

    Inicialización del repositorio

---
+++ src/github_matrices/Github_Matrices.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github_matrices;
+//import java.util.Scanner;
+/**
+ *
+ * @author Tayri
+ */
+public class Github_Matrices {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        
+        String menu = "¿Que Matriz quieres utilizar? \n 1: Matriz 3x3 \n 2: Matriz 4x4 \n 3: Matriz 5x5 " ;
+        
+        int LX3x3 = 3;
+        int LX4x4 = 4;
+        int LX5x5 = 5;
+        
+        int LY3x3 = 3;
+        int LY4x4 = 4;
+        int LY5x5 = 5;
+    }
+ 
+    }
+    
